f6bc72843c5122e260ec572afba94297d5a61d70
Javier Rodriguez (ADNOC IT) <javier.rodriguez@clearpeaks.com>
2019-01-24 16:43:32 +0100

    First commit

---
+++ src/utils.js
@@ -0,0 +1,140 @@
+export const hierarchyData = {
+  address: [
+    'zipCode',
+    'city',
+    'cityPrefix',
+    'citySuffix',
+    'streetName',
+    'streetAddress',
+    'streetSuffix',
+    'streetPrefix',
+    'secondaryAddress',
+    'county',
+    'country',
+    'countryCode',
+    'state',
+    'stateAbbr',
+    'latitude',
+    'longitude'
+  ],
+  commerce: [
+    'color',
+    'department',
+    'productName',
+    'price',
+    'productAdjective',
+    'productMaterial',
+    'product'
+  ],
+  company: [
+    'suffixes',
+    'companyName',
+    'companySuffix',
+    'catchPhrase',
+    'bs',
+    'catchPhraseAdjective',
+    'catchPhraseDescriptor',
+    'catchPhraseNoun',
+    'bsAdjective',
+    'bsBuzz',
+    'bsNoun'
+  ],
+  date: [
+    'past',
+    'future',
+    'between',
+    'recent',
+    'soon',
+    'month',
+    'weekday'
+  ],
+  finance: [
+    'account',
+    'accountName',
+    'mask',
+    'amount',
+    'transactionType',
+    'currencyCode',
+    'currencyName',
+    'currencySymbol',
+    'bitcoinAddress',
+    'ethereumAddress',
+    'iban',
+    'bic'
+  ],
+  internet: [
+    'avatar',
+    'email',
+    'exampleEmail',
+    'userName',
+    'protocol',
+    'url',
+    'domainName',
+    'domainSuffix',
+    'domainWord',
+    'ip',
+    'ipv6',
+    'userAgent',
+    'color',
+    'mac',
+    'password'
+  ],
+  lorem: [
+    'word',
+    'words',
+    'sentence',
+    'slug',
+    'sentences',
+    'paragraph',
+    'paragraphs',
+    'text',
+    'lines'
+  ],
+  name: [
+    'firstName',
+    'lastName',
+    'findName',
+    'jobTitle',
+    'prefix',
+    'suffix',
+    'title',
+    'jobDescriptor',
+    'jobArea',
+    'jobType'
+  ],
+  phone: [
+    'phoneNumber',
+    'phoneNumberFormat',
+    'phoneFormats'
+  ],
+  random: [
+    'number',
+    'float',
+    'arrayElement',
+    'objectElement',
+    'uuid',
+    'boolean',
+    'word',
+    'words',
+    'image',
+    'locale',
+    'alphaNumeric',
+    'hexaDecimal'
+  ],
+  system: [
+    'fileName',
+    'commonFileName',
+    'mimeType',
+    'commonFileType',
+    'commonFileExt',
+    'fileType',
+    'fileExt',
+    'directoryPath',
+    'filePath',
+    'semver'
+  ]
+};
+
+
+
+
